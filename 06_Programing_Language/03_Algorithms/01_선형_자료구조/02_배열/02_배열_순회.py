0410aeeb273d6efa1da050dea74c353a34cc580b
whale2200d <whale2200d@gmail.com>
2026-09-09 08:16:38 +0900

    algorithms: 배열 2 (배열 순회)

---
+++ 06_Programing_Language/03_Algorithms/01_선형_자료구조/02_배열/02_배열_순회.py
@@ -0,0 +1,25 @@
+# 순회 및 탐색(traversal / linear search)
+# 배열의 처음부터 끝까지 순서대로 요소에 접근하는 것이 순회
+# 순회 중 특정 조건을 만족하는 요소를 찾는 것이 선형 탐색
+# 정렬 여부와 부관하게 적용 가능
+# 시간복잡도는 O(N)
+
+# 정수 배열과 목표값이 주어질 때, 배열 내에서 목표값과 일치하는 첫 번째 요소의 인덱스를 반환하라.
+# 존재하지 않으면 -1을 반환하라.
+input_array = [4, 2, 7, 1, 9]
+target = 7
+output_result = 2
+
+def solution(input_array: list[int], target: int) -> int:
+    # 반복문: 배열을 순회하며 조건에 부합하는 목표값 탐색
+    for index in range(len(input_array)):
+        # 조건문: 목표값과 일치하는 값이 있는지 탐색
+        if input_array[index] == target:
+            # 조건 만족 시: 해당 인덱스 반환
+            return index
+
+    # 반복을 종료까지 목표값을 탐색하지 못했다면, -1 반환
+    return -1
+
+print(solution(input_array, target))
+print(output_result)
